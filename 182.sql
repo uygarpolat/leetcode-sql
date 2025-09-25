b20c5cd3d88b16cf6b9cbfb62755b97f5cd1380c
uygarpolat <uygarpolat@gmail.com>
2025-09-26 02:52:19 +0300

    182.sql

---
+++ 182.sql
@@ -0,0 +1,52 @@
+/*
+182. Duplicate Emails
+Easy
+Topics
+premium lock icon
+Companies
+SQL Schema
+Pandas Schema
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+Write a solution to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Person table:
++----+---------+
+| id | email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+Output: 
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+Explanation: a@b.com is repeated two times.
+*/
+SELECT DISTINCT(p1.email)
+FROM Person AS p1
+INNER JOIN Person as p2
+ON p1.email = p2.email
+AND p1.id != p2.id
