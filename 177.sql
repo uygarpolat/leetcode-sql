438efc03164bedb7ea8eda0c86a13fb39a34c50b
uygarpolat <uygarpolat@gmail.com>
2025-09-26 02:09:44 +0300

    177.sql

---
+++ 177.sql
@@ -0,0 +1,73 @@
+/*
+177. Nth Highest Salary
+
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the nth highest distinct salary from the Employee table. If there are less than n distinct salaries, return null.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+n = 2
+Output: 
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| 200                    |
++------------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+n = 2
+Output: 
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| null                   |
++------------------------+
+*/
+CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS $$
+BEGIN
+  RETURN QUERY
+  SELECT (
+    CASE 
+      WHEN N <= 0 THEN NULL
+      ELSE (
+        SELECT DISTINCT e.salary
+        FROM Employee AS e
+        ORDER BY e.salary DESC
+        OFFSET N - 1
+        LIMIT 1
+      )
+    END
+  ) AS "Salary";
+END;
+$$ LANGUAGE plpgsql;
